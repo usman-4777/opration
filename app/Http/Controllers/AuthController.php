2515a4a4c6af9fda9f8123d19d85e0ceacf944e0
usman <ashrafusman768@gmail.com>
2022-12-17 22:37:00 +0500

    comment added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,6 +14,7 @@ class AuthController extends Controller
            'email' => 'required|email',
            'password' => 'required'
         ]);
+        // this is just comment
         $user = User::where('email', $request->email)->first();
         //$otp_code = random_int(100000, 999999);
         //$user->(['otp_code' => $otp_code])->save();
